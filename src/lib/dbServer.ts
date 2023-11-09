eecf4542378ec8dfa333704579396b1a177eff75
Adrian Nico Salvado <adriannicosalvado@gmail.com>
2023-11-09 11:34:17 -0500

    removed unused variable

--- src/lib/dbServer.ts
+++ src/lib/dbServer.ts
@@ -6,7 +6,6 @@ export const POCKET_BASE_URL = "http://127.0.0.1:8090";
 export class DatabaseServer {
   client: PocketBase;
   clientId?: string;
-  authenticated: boolean = false;
 
   constructor() {
     this.client = new PocketBase(POCKET_BASE_URL);
