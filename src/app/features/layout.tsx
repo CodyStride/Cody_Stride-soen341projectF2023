21c134fe959aba536c2eb158fbeecfaf1698d8af
AdamYoshi <adamchami012@gmail.com>
2023-11-16 18:45:32 -0500

    Fixed the broker search

--- src/app/features/layout.tsx
+++ src/app/features/layout.tsx
@@ -1,36 +1,56 @@
-'use client';
+"use client";
 
-import { BiHomeAlt } from "react-icons/bi"
-import { Button, Group, AppShell, Burger } from '@mantine/core';
+import { BiHomeAlt } from "react-icons/bi";
+import { Button, Group, AppShell, Burger } from "@mantine/core";
 
 export default function FeaturePage({ children }: { children: any }) {
-  return (<AppShell
-    header={{ height: 60 }}
-    // navbar={{ width: 300, breakpoint: 'sm' }}
-    padding="md"
-  >
-    <AppShell.Header>
-      <Group h="100%" gap={0} >
-        <Group justify="center" pb="xl" px="md" >
-          <Burger hiddenFrom="sm" size="sm" />
-          <Button variant="outline" size="lg" component="a" href="/features">
-            <BiHomeAlt />
-          </Button>
-          <Button variant="outline" size="lg" component="a" href="/features/search">
-            View Listings
-          </Button>
-          {/* <Button variant="outline" size="lg" component="a" href="/dashboard">
-          Brokers
-        </Button> */}
-          <Button variant="outline" size="lg" component="a" href="/features/broker">
-            Properties
-          </Button>
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      // navbar={{ width: 300, breakpoint: 'sm' }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" gap={0}>
+          <Group justify="center" pb="xl" px="md">
+            <Burger hiddenFrom="sm" size="sm" />
+            <Button variant="outline" size="lg" component="a" href="/features">
+              <BiHomeAlt />
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              component="a"
+              href="/features/search"
+            >
+              View Listings
+            </Button>
+            <Button variant="outline" size="lg" component="a" href="/features/dashboard">
+              Dashboard
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              component="a"
+              href="/features/broker"
+            >
+              Properties
+            </Button>
+            <Button
+              variant="outline"
+              size="lg"
+              component="a"
+              href="/features/broker_search"
+            >
+              System Admin Page
+            </Button>
+          </Group>
         </Group>
-      </Group>
-    </AppShell.Header>
+      </AppShell.Header>
 
-    {/* <AppShell.Navbar p="md">Navbar</AppShell.Navbar> */}
+      {/* <AppShell.Navbar p="md">Navbar</AppShell.Navbar> */}
 
-    <AppShell.Main>{children}</AppShell.Main>
-  </AppShell>)
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
 }
